c673c84f1718c4110bda07b8002aed721f220bcb
Nasibo Yussuf <yussufnasibo@gmail.com>
2019-04-30 09:46:01 +0300

    added function to script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function getTriangle(){
+    var a = parseFloat(prompt("side a"));
+    var b = parseFloat(prompt("side b"));
+    var c = parseFloat(prompt("side c"));
+    var determine = function(a,b,c){
+        if a===b && b===c && a===c{
+            alert("Equilateral triangle")
+        }
+    }
+
+}
