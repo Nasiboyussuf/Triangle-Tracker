d8e8386a61b01e8132b8dde135989c60c794f471
Nasibo Yussuf <yussufnasibo@gmail.com>
2019-05-02 13:47:38 +0300

    added flex box,background -image and a form

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-function getTriangle(){
-    var a = parseFloat(prompt("side a"));
-    var b = parseFloat(prompt("side b"));
-    var c = parseFloat(prompt("side c"));
+function getTriangle() {
+    var a = parseInt(document.getElementById('length1').value);
+    var b = parseInt(document.getElementById('length2').value);
+    var c = parseInt(document.getElementById('length3').value);
 
+   
     //  var determine = function(a,b,c){
- if(a+b<=c||a+c<=b||b+c<=a){
-            alert("Not a triangle");
- }
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        alert("That is Not a triangle");
+    }
 
-   else if( a==b && b==c && a==c){
-            alert("Equilateral triangle");
-        }
-     
-        else if(a===b || a===c || b===c){
-            alert("Isosceles triangle");
-        }
-        else if(a+b<=c||a+c<=b||b+c<=a){
-            alert("Not a triangle");
+    else if (a == b && b == c && a == c) {0
+        alert("That is an Equilateral triangle");
+    }
 
-        }
-        else {
-            alert ("Triangle is Scalene");
-        }
+    else if (a === b || a === c || b === c) {
+        alert("That is an Isosceles triangle");
+    }
+    // else if (a + b <= c || a + c <= b || b + c <= a) {
+    //     alert("That is Not a triangle");
 
-        }
+    // }
+    else if(a!==b!==c) {
+        alert("The Triangle is Scalene");
+    }
+    else{
+        alert('Invalid input');
+    }
 
-    
+};
+
